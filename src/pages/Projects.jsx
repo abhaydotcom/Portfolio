4c478b1d636a4d787ad7579dd16d5a673fa1d05b
Abhay kumar singh <abhaykumarsingh2904@gmail.com>
2025-11-22 15:32:53 +0530

    Add my Project info

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,56 +1,53 @@
 import { ExternalLink, Github } from 'lucide-react'
 import React from 'react'
+import bussinesPage from '../../public/BusinesLandingPage.jpg'
+import Blogz from '../../public/Blogz.jpg'
+import foodiecart from '../../public/foodiecart.jpg'
+import Movieflix from '../../public/Movieflix.jpg'
+import WeatherWeb from '../../public/WeatherWeb.jpg'
 
 
 const project=[
-  { title: "Student Admin Panel",
+  { title: "Business Landing Page",
     description:
-      "A school website built with React, for showcasing the school in the upper level.",
-    tech: ["React", "Node.js", "MongoDB", "TailwindCSS"],
-    image:
-      "https://res.cloudinary.com/dkrwq4wvi/image/upload/v1762506417/project%20school.jpg",
-    demo: "https://rhacademy.in",
-    github: "https://github.com/fahimlisl/rhacademy",
+      " A Business Solutions Landing Page with React, TailwindCSS, Framer Motion, SwiperJS, and React Icons to deliver a modern, responsive, and engaging user experience.",
+    tech: ["React", "Node.js", "MongoDB", "TailwindCSS","Framer-Motion"],
+    image:bussinesPage,
+    demo: "https://businesslandingvite.vercel.app",
+    github: "https://github.com/abhaydotcom/BusinessLandingPage",
   },
     {
-    title: "Portfolio Website",
-    description:
-      "My personal developer portfolio — built with React, Tailwind, Express, Framer Motion, and MongoDB. Features animations, dark mode, and backend integration.",
+    title: "Full Stack Blogz",
+    description:"A comprehensive MERN Blog Platform!,A feature-rich blogging application that combines modern web technologies with an intuitive user experience",
+    tech: ["React", "Node","Express","Redux","MongoDb", "Tailwind"],
+    image:Blogz,
+    demo: "https://mernblogz.vercel.app/",
+    github: "https://github.com/abhaydotcom/Mern-Blog",
+  },
+   {
+    title: "Movie Website",
+    description:"A React movie app featuring live API data, movie search, detailed info pages, YouTube trailer playback, and optimized pagination.",
     tech: ["React", "Framer Motion", "Tailwind"],
-    image:
-      "https://res.cloudinary.com/dkrwq4wvi/image/upload/v1762710961/myport.jpg",
-    demo: "https://fahim.in",
-    github: "https://github.com/fahimlisl/myPortfolio",
+    image:Movieflix,
+    demo: "https://movflix4u.vercel.app/",
+    github: "https://github.com/abhaydotcom/MovFlix",
   },
     {
-    title: "Portfolio Website",
-    description:
-      "My personal developer portfolio — built with React, Tailwind, Express, Framer Motion, and MongoDB. Features animations, dark mode, and backend integration.",
-    tech: ["React", "Framer Motion", "Tailwind"],
-    image:
-      "https://res.cloudinary.com/dkrwq4wvi/image/upload/v1762710961/myport.jpg",
-    demo: "https://fahim.in",
-    github: "https://github.com/fahimlisl/myPortfolio",
+    title: "Weather Website",
+    description:"A modern weather experience built with React, TypeScript,TanStack query, and ShadCN.",
+     tech: ["React", "TypeScript","ShadCn","TanStack", "Tailwind"], 
+    image:WeatherWeb,
+    demo: "https://climatetsx.vercel.app/",
+    github: "https://github.com/abhaydotcom/Weather-tanstack-tsx",
   },
+ 
   {
-    title: "Portfolio Website",
-    description:
-      "My personal developer portfolio — built with React, Tailwind, Express, Framer Motion, and MongoDB. Features animations, dark mode, and backend integration.",
+    title: "FoodieCart",
+    description:"A responsive food ordering UI created using React and Tailwind, offering smooth navigation and a clean user experience.",
     tech: ["React", "Framer Motion", "Tailwind"],
-    image:
-      "https://res.cloudinary.com/dkrwq4wvi/image/upload/v1762710961/myport.jpg",
-    demo: "https://fahim.in",
-    github: "https://github.com/fahimlisl/myPortfolio",
-  },
-  {
-    title: "Portfolio Website",
-    description:
-      "My personal developer portfolio — built with React, Tailwind, Express, Framer Motion, and MongoDB. Features animations, dark mode, and backend integration.",
-    tech: ["React", "Framer Motion", "Tailwind"],
-    image:
-      "https://res.cloudinary.com/dkrwq4wvi/image/upload/v1762710961/myport.jpg",
-    demo: "https://fahim.in",
-    github: "https://github.com/fahimlisl/myPortfolio",
+    image:foodiecart,
+    demo: "https://foodiecart.vercel.app/",
+    github: "https://github.com/abhaydotcom/Foodie-Kart",
   },
   
 ]
@@ -82,7 +79,7 @@ const Projects = () => {
       >
 
         {
-          project.map((card,i)=>(
+          project?.map((card,i)=>(
             <div 
             key={i}
             className='group relative bg-white/60 dark:bg-gray-800/60 backdrop-blur-xl border border-gray-200 dark:border-gray-700 rounded-2xl shadow-md overflow-hidden hover:shadow-blue-500/20 transition-all '
@@ -91,10 +88,11 @@ const Projects = () => {
               className='h-52 overflow-hidden'
               >
                 <img
-                src={card.image}
-                alt={`${card.title} preview`}
+                
+                src={card?.image}
+                alt={`${card?.title} preview`}
                 loading='lazy'
-                className='w-full h-full object-cover group-hover:scale-110 transition-transform duration-500'
+                className='w-full h-full p-2 rounded-2xl object-cover group-hover:scale-110 transition-transform duration-500'
                 />
               </div>
 
@@ -105,20 +103,20 @@ const Projects = () => {
                   <h3 
                   className='font-bold text-xl py-2'
                   >
-                    {card.title}
+                    {card?.title}
                   </h3>
 
                   <p
                   className="text-gray-600 dark:text-gray-300 text-sm mb-4"
                   >
-                    {card.description}
+                    {card?.description}
                   </p>
 
                   <div 
                   className='flex flex-wrap gap-2 mb-4 '
                   >
 
-                    {card.tech.map((item,i)=>(
+                    {card?.tech?.map((item,i)=>(
                       <span 
                       key={i}
 
